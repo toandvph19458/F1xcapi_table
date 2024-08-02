6d0121d7b02b2c971ee7886b2d1ae639c13ab374
Duong Van Toan <105204332+toandvph19458@users.noreply.github.com>
2024-08-02 14:00:02 +0700

    1

--- src/views/table-price/table-2.vue
+++ src/views/table-price/table-2.vue
@@ -118,20 +118,46 @@
                     </tbody>
                 </table>
             </div>
+            <div class="chart__right">
+                <Bar :data="chartData" :options="chartOptions"/>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, watch } from "vue";
 import TableHeader from "../../components/table-price/table-header.vue";
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default defineComponent({
     name: "tabel-2",
     components: {
         TableHeader,
+        Bar
     },
-    setup(props, ctx) { },
+   
+    setup(props, ctx) { 
+        const chartData = ref({
+            labels: ['January', 'February', 'March'],
+            datasets: [{ data: [40, 20, 12] }]
+        });
+
+        const chartOptions = ref({
+            responsive: true,
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        });
+        return {
+            chartData,
+            chartOptions
+        }
+    },
 });
 </script>
 
@@ -146,70 +172,77 @@ export default defineComponent({
     .tabel__inner {
         max-width: 1760px;
         margin: 0 auto;
+        display: flex;
+        gap: 24px;
+        table{
+            width: 100%;
+        }
         .table-bordered {
-            thead{
-                tr{
-                    th{
-                        border: 1px solid #1C163F;
+                thead {
+                    tr {
+                        th {
+                            border: 1px solid #1C163F;
+                        }
+                    }
+                }
+
+                tbody {
+                    tr {
+                        td {
+                            border: 1px solid #1C163F;
+                        }
                     }
                 }
             }
-            tbody{
-                tr{
-                    td{
-                        border: 1px solid #1C163F;
-                    }
-                }
+
+            .table__price .table-dark {
+                background-color: #181336 !important;
+                border-radius: 12px !important;
+                margin-bottom: 0;
             }
-        }
-        .table__price .table-dark {
-            background-color: #181336 !important;
-            border-radius: 12px !important;
-            margin-bottom: 0;
-        }
 
-        .table__price .table-dark.table-striped tbody tr:nth-of-type(odd) {
-            background-color: #1c163f !important;
-        }
+            .table__price .table-dark.table-striped tbody tr:nth-of-type(odd) {
+                background-color: #1c163f !important;
+            }
 
-        .table__price .table-dark.table-striped thead tr th {
-            border-top: none;
-            font-size: 12px;
-            font-weight: 600;
-            line-height: 16px;
-        }
+            .table__price .table-dark.table-striped thead tr th {
+                border-top: none;
+                font-size: 12px;
+                font-weight: 600;
+                line-height: 16px;
+            }
 
-        .table__price .table-dark.table-striped tbody tr td {
-            border-bottom: none;
-        }
+            .table__price .table-dark.table-striped tbody tr td {
+                border-bottom: none;
+            }
 
-        .table__price .table-dark.table-striped tbody tr td {
-            font-size: 12px;
-            font-weight: 400;
-            line-height: 16px;
-        }
+            .table__price .table-dark.table-striped tbody tr td {
+                font-size: 12px;
+                font-weight: 400;
+                line-height: 16px;
+            }
 
-        .table__price .table-dark.table-striped thead tr th:first-child {
-            border-left: none !important;
-            border-radius: 12px 0;
-        }
+            .table__price .table-dark.table-striped thead tr th:first-child {
+                border-left: none !important;
+                border-radius: 12px 0;
+            }
 
-        .table__price .table-dark.table-striped tbody tr td:first-child {
-            border-left: none !important;
-        }
+            .table__price .table-dark.table-striped tbody tr td:first-child {
+                border-left: none !important;
+            }
 
-        .table__price .table-dark.table-striped thead tr th:last-child {
-            border-right: none !important;
-            border-radius: 0px 12px;
-        }
+            .table__price .table-dark.table-striped thead tr th:last-child {
+                border-right: none !important;
+                border-radius: 0px 12px;
+            }
 
-        .table__price .table-dark.table-striped tbody tr td:last-child {
-            border-right: none !important;
-        }
+            .table__price .table-dark.table-striped tbody tr td:last-child {
+                border-right: none !important;
+            }
 
-        .table__price .table-dark .table-striped tbody tr:last-child>td:first-child {
-            border-radius: 12px;
-        }
+            .table__price .table-dark .table-striped tbody tr:last-child>td:first-child {
+                border-radius: 12px;
+            }
     }
 }
 </style>
